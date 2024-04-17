c0c7ef32534b1783b699b7c90b19e560993bd6d7
Andres Urrego <andres.urrego@polygon.us>
2024-04-17 18:15:14 -0500

    - gizmo shooter

--- Assets/_Scripts/ShootController.cs
+++ Assets/_Scripts/ShootController.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class ShootController : MonoBehaviour
 {
+    [Header("Hitscan")]
+    [SerializeField] private float hitScanRange = 100f;
+    [SerializeField] private Transform root;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(root.position, root.position + root.forward * hitScanRange);
+    }
+
     private void OnEnable()
     {
         PlayerInputActions.OnFireInput += OnFire;
